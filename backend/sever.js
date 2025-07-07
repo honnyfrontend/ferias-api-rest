f634f65219fb15984770b4d9842fb0710494e3ab
honny batista de oliveira <93882631+honnyfrontend@users.noreply.github.com>
2025-07-07 10:08:15 -0300

    voltei ao simples

--- backend/sever.js
+++ backend/sever.js
@@ -2,8 +2,7 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+
 app.use(cors({
   origin: 'https://ferias-api-rest-3.onrender.com'
 }));
@@ -14,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+
 
 // Conexão com MongoDB
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/usuariosdb', {
@@ -26,26 +25,12 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/usuariosdb'
 const UsuarioSchema = new mongoose.Schema({
   nome: String,
   email: String,
-  portfolio: [{ url: String }],
+  
 });
 const Usuario = mongoose.model('Usuario', UsuarioSchema);
 
 
-app.get('/usuarios/:id/portfolio', async (req, res) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).json({ mensagem: 'ID inválido' });
-  }
-  const usuario = await Usuario.findById(req.params.id);
-  if (!usuario) return res.status(404).json({ mensagem: 'Usuário não encontrado' });
-  res.json(usuario.portfolio || []);
-});
 
-// algo
-app.post('/upload', upload.single('foto'), async (req, res) => {
-  if (!req.file) return res.status(400).json({ mensagem: 'Nenhuma foto enviada' });
-  const fotoUrl = `/uploads/${req.file.filename}`;
-  res.json({ url: fotoUrl });
-});
 
 // Listar todos os usuários
 app.get('/usuarios', async (req, res) => {
